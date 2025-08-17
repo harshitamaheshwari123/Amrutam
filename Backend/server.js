ae63e7828717731f1c900c9b6f33abe25cd8c404
Harshita <harshitamaheshwari762@gmail.com>
2025-08-18 00:49:08 +0530

    final added

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
 const doctorRoutes = require("./routes/doctors");
 const appointmentRoutes = require("./routes/appointments");
 const cors = require("cors");
 
-dotenv.config();
-
 const app = express();
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://amrutam1.onrender.com/",
+  })
+);
+
 app.use(express.json());
 
 app.use("/api", authRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/appointments", appointmentRoutes);
-
 
 mongoose
   .connect(process.env.MONGO_URI)
